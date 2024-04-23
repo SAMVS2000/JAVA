a138801c5451754efca5092b5c55bbdab3d6768f
SAM V S <140595415+SAMVS2000@users.noreply.github.com>
2024-04-23 11:07:22 +0530

    Add files via upload

---
+++ FWriter.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class FWriter
+{
+	public static void main(String args[])
+    {
+        FileWriter fw;
+        BufferWriter bw;
+        try
+        {
+            fw= new FileWriter(new File("a.txt"));
+            bw= new BufferWriter(fw);
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter data / quit to terminate ");
+            String data = s.nextLine();
+            while(!data.equals("quit"))
+            {
+                bw.write(data + "\n");
+                data = s.nextLine();
+            }
+            bw.close();
+        } 
+        catch(Exception e)
+        {
+            System.out.println("Error : " + e);
+        }
+    }
+}
