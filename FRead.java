cac1e9fcf250436dbdd42c85dd6fb6407fe8cb27
SAM V S <140595415+SAMVS2000@users.noreply.github.com>
2024-05-01 15:33:59 +0530

    Add files via upload

---
+++ FRead.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+class FRead
+{
+	public static void main(String args[])
+    {
+        FileInputStream fin;
+	    Scanner s = new Scanner(System.in);
+	    String fname;
+        try
+        {
+            System.out.println("Enter the Filename : ");
+            fname=s.nextLine();
+            fin=new FileInputStream(fname);
+            int ch;
+            ch = fin.read();
+            while(ch!= -1)
+            {
+                System.out.print((char)ch);
+                ch = fin.read();
+            }
+            fin.close();
+        } 
+        catch(Exception e)
+        {
+            System.out.println("Error : " + e);
+        }
+    }
+}
